f1d02ee0fd0d29b31ce28a9b7abf460b6701ce0d
guangliu <k1466006376@163.com>
2025-08-23 12:00:49 +0800

    feat: 添加史莱姆敌人和更新玩家控制
    - 新增史莱姆敌人场景和脚本，包含移动和碰撞检测功能
    - 更新玩家控制脚本，使用自定义输入映射并添加动画
    - 修改killzone脚本，添加慢动作效果和碰撞移除
    - 添加新的输入映射配置

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,14 +11,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
+		animated_sprite_2d.flip_h = direction == -1
 		velocity.x = direction * SPEED
+			
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
